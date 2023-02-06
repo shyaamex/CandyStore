c576a80cd11addf1a4f9d15de3142624a292c313
Shyaam <115785301+shyaamex@users.noreply.github.com>
2023-02-06 10:39:25 +0530

    URLs modified

--- CandyStore/settings.py
+++ CandyStore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'e+e#vq+j1^$m+ipl$m6n$&fk!k!ewtkoqe$4--!#tfvx&j5w#&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
